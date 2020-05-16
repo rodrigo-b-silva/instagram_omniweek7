7a59921cb97594f8f06fe72454a5e65ee1000804
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-05-16 13:32:55 -0300

    Conclusão app

---
+++ app/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { Image } from 'react-native'
+
+import logo from './assets/logo.png';
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+export default createAppContainer(
+    createStackNavigator({
+        Feed,
+        New
+    }, {
+        defaultNavigationOptions: {
+            headerTintColor: '#000',
+            headerTitle: <Image style={{ marginHorizontal: 20 }} source={logo} />,
+            headerBackTitle: null
+        },
+        mode: 'modal'
+    })
+);
